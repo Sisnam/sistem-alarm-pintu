402b5ed4c2bab854e15b579a37102784937a71ec
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-10-24 20:10:44 +0700

    feat: add solution

--- sistem_alarm_pintu/src/main.c
+++ sistem_alarm_pintu/src/main.c
@@ -4,6 +4,7 @@
 #include <board.h>
 
 static char strbuf[128];
+volatile bool system_active = false;
 volatile bool door_open = false;
 volatile bool alarm_active = false;
 volatile int counter = 0;
@@ -35,20 +36,19 @@ void PWM_Init(void)
 // ISR untuk SW0 (PORTF PIN1)
 ISR(PORTF_INT0_vect)
 {
-	door_open = !door_open;	  // Toggle status pintu
-	alarm_active = door_open; // Set status alarm berdasarkan status pintu
-	counter = 0;			  // Reset counter setiap kali status berubah
+	system_active = !system_active;
+	counter = 0;     // Reset counter setiap kali status berubah
 }
 
 // Inisialisasi interrupt eksternal untuk SW0
 void init_interrupts(void)
 {
-	PORTF.DIRCLR = PIN1_bm;				  // Set PIN1 sebagai input (SW0)
+	PORTF.DIRCLR = PIN1_bm;      // Set PIN1 sebagai input (SW0)
 	PORTF.PIN1CTRL = PORT_ISC_FALLING_gc; // Detect falling edges
-	PORTF.INT0MASK = PIN1_bm;			  // Enable interrupt untuk PIN1
-	PORTF.INTCTRL = PORT_INT0LVL_LO_gc;	  // Set level interrupt ke low
-	PMIC.CTRL |= PMIC_LOLVLEN_bm;		  // Enable low-level interrupts
-	cpu_irq_enable();					  // Enable global interrupts
+	PORTF.INT0MASK = PIN1_bm;     // Enable interrupt untuk PIN1
+	PORTF.INTCTRL = PORT_INT0LVL_LO_gc;   // Set level interrupt ke low
+	PMIC.CTRL |= PMIC_LOLVLEN_bm;    // Enable low-level interrupts
+	cpu_irq_enable();       // Enable global interrupts
 }
 
 void set_on_led(void)
@@ -68,9 +68,22 @@ void set_flicker_led(void)
 	LED_On(LED1);
 }
 
+void check_door_sensor(void)
+{
+	bool is_closed = PORTE.IN & PIN0_bm;
+	
+	if (is_closed) {
+		door_open = false;
+	}
+	else{
+		door_open = true;
+	}
+}
+
 void init_door_sensor(void)
 {
-	
+	PORTE.DIRCLR = PIN0_bm;
+	PORTE.PIN0CTRL = PORT_OPC_PULLUP_gc;
 }
 
 int main(void)
@@ -96,33 +109,49 @@ int main(void)
 	
 	while (true)
 	{
-		 // Mengecek status sensor pintu
-		 bool is_closed = !(PORTC.IN & PIN2_bm);
-		 snprintf(strbuf, sizeof(strbuf), "closed: %s", is_closed ? "true" : "false");
-		 gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
-		 
-		 
-		if (alarm_active)
+		// Mengecek status sensor pintu
+		check_door_sensor();
+		snprintf(strbuf, sizeof(strbuf), "Status Pintu: %s", door_open ? "Closed" : "Open");
+		gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+		
+		if (system_active)
 		{
 			// Tampilkan status pintu dan counter di LCD
-			gfx_mono_draw_string("Pintu Terbuka ", 0, 8, &sysfont);
+			gfx_mono_draw_string("Sistem Aktif   ", 0, 8, &sysfont);
 			snprintf(strbuf, sizeof(strbuf), "Waktu: %02d detik", counter);
-			gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+			gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
+			
+			
+			if (!door_open){
+				alarm_active = true;
+					
+				// Kedip LED0 dan LED1 jika lebih dari 10 detik
+				if (counter < 10)
+				{
+					TCC0.CCA = 800;
+					set_on_led();
+				}
+				else
+				{
+					TCC0.CCA = 1000;
+					set_flicker_led();
+				}
 
-			// Kedip LED0 dan LED1 jika lebih dari 10 detik
-			if (counter < 10)
-			{
-				TCC0.CCA = 800;
-				set_on_led();
+				// Tambah counter setiap detik
+				counter++;				
 			}
-			else
-			{
-				TCC0.CCA = 1000;
-				set_flicker_led();
-			}
+			else if (!alarm_active){
+				// Jika alarm tidak aktif (pintu tertutup)
+				// Reset semua
+				TCC0.CCA = 0;
+				LED_Off(LED0);
+				LED_Off(LED1);
+				counter = 0;
 
-			// Tambah counter setiap detik
-			counter++;
+				gfx_mono_draw_string("Sistem Nonaktif", 0, 8, &sysfont); // Update status di LCD
+				snprintf(strbuf, sizeof(strbuf), "Waktu: %02d detik", counter);
+				gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
+			}
 		}
 		else
 		{
@@ -133,9 +162,9 @@ int main(void)
 			LED_Off(LED1);
 			counter = 0;
 
-			gfx_mono_draw_string("Pintu Tertutup ", 0, 8, &sysfont); // Update status di LCD
+			gfx_mono_draw_string("Sistem Nonaktif", 0, 8, &sysfont); // Update status di LCD
 			snprintf(strbuf, sizeof(strbuf), "Waktu: %02d detik", counter);
-			gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+			gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
 		}
 	}
 }
